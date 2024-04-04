e96340ae9eb77fe43111d20090f6cc05c8ae4eff
HUY <ngoduchuy888@gmail.com>
2024-04-04 20:33:48 +0700

    detail

---
+++ controllers/productController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function ProductsShow($id)
+{
+
+    $products = showOneForProduct($id);
+    if (empty($products)) {
+        e404();
+    }
+
+    $fileNameShow = [
+        'p_name' => 'Tên sản phẩm',
+        'p_price' => 'Giá',
+        'p_sale_price' => 'Giá sale',
+        'p_description' => 'Mô tả',
+        'p_pimage' => 'Hình ảnh',
+        'p_img_thumbnail' => 'Biến thể',
+        'p_quantity' => 'Số lượng',
+        'p_key_word' => 'Từ khóa',
+        'p_view' => 'Lượt xem',
+        'p_id' => 'Mã sản phẩm ',
+        'p_id_brand' => 'Mã nhãn hàng',
+        'p_id_category' => 'Mã loại',
+        'c_name' => 'Tên loại',
+        'au_name' => 'Tên nhãn hàng',
+    ];
+
+    $view = 'contents/product-detail';
+
+    require_once PATH_VIEW . "master.php";
+}
+
+
